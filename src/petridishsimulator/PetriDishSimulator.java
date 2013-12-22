545623797743acec971f5a8361332c0001d05a22
Reicher <robin.o.reicher@gmail.com>
2013-12-22 11:26:57 +0100

    initial commit

---
+++ src/petridishsimulator/PetriDishSimulator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petridishsimulator;
+
+/**
+ *
+ * @author regen
+ */
+public class PetriDishSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
